1ee8993c40d8bad1e4266f2ca1b4b3ae047f8f45
Johnny Estrada <johnnye562@yahoo.com>
2024-09-30 23:49:19 -0700

    Update Login page height to screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
     <>
-      <div className="flex min-h-full flex-1">
+      <div className="flex min-h-screen flex-1">
         <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
